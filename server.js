82536bfd247b47744d315d9ef3b8927b47b8142b
icesunflower <baosi@dz.tt>
2018-04-23 17:04:44 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http=require('http');
+
+const hostname = '127.0.0.1';
+const port = 8082;
+
+
+
+exports.start  = () => {
+    const server = http.createServer((req,res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','text/plain');
+        res.end('hello world\n');
+    });
+
+    server.listen(port,hostname,() => {
+        console.log(`服务器运行在 http://${hostname}:${port}/`)
+    })
+}
+
