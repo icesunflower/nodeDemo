f67ce3b7e843aa2bd4457f490394b4c234d1532f
icesunflower <baosi@dz.tt>
2018-04-24 09:53:49 +0800

    study copy

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const server = require('./server');
+var fs = require('fs');
 
-server.start()
+
+fs.copyFile('copy/one.txt', 'copy/two.txt', (err) => {
+    if (err) throw err;
+    console.log('source.txt was copied to destination.txt');
+});
+
+server.start();
+
+
